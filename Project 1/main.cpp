0341695311644a7452564641d2cd99c1730c4f99
Shan Qamruddin <Home@s172-20-118-h126.paws.uga.edu>
2018-02-12 13:49:05 -0500

    rawr

--- Project 1/main.cpp
+++ Project 1/main.cpp
@@ -18,6 +18,8 @@ void insertValue(){
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Commands,\n \t(i) - Insert value \n \t(d) - Delete value \n \t(f) - Search value \n \t(n) - Print iterator's next value \n \t(r) - Reset iterator \n \t(p) - Print list \n \t(l) - Print length \n \t(s) - Parewise Swap \n \t(c) - Clear List \n \t(q) - Quit Program \n\n Enter a command:";
+    
+    
    
     x = getchar();
     if(x == 'i'){
