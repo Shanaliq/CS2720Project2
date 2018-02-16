c42c2d96fee098ca85143486481a61ce294852fd
Jackson Holiman <jholiman@greencourt.com>
2018-02-15 20:25:16 -0500

    changes to SortedLinkedList

--- Project 1/SortedLinkedList.cpp
+++ Project 1/SortedLinkedList.cpp
@@ -9,6 +9,49 @@
 #include "SortedLinkedList.h"
 
 
+SortedLinkedList::SortedLinkedList() {
+	count = 0;
+	head = NULL;
+}
+
+SortedLinkedList::~SortedLinkedList() {
+	ListNode * tempPtr;
+	while (head != NULL) {
+		tempPtr = head;
+		head = head -> next;
+		delete tempPtr;
+	}
+	count = 0;
+}
+
+int SortedLinkedList::length() const {
+	return count;
+}
+
+void SortedLinkedList::insertItem(DataType & item) {
+	ListNode * input = new ListNode(item);
+	ListNode * currentPos;
+	if (head == NULL || head->item.getValue() >= input ->item.getValue()) {
+		input->next = head;
+		head = input;
+	}
+	else{
+		currentPos = head;		
+		while (input->next != NULL && currentPos->next->item.getValue() < input->item.getValue()) {
+			currentPos = currentPos->next;
+		}
+		input->next = currentPos->next;
+		currentPos->next = input;
+
+	}
+}
+
+
+
+
+	
+
+
 
 
 
