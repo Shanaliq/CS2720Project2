8e7262703dfa513904478b67af88983b4a2040fa
Jackson Holiman <jholiman@greencourt.com>
2018-02-18 16:58:22 -0500

    input

--- Project 1/main.cpp
+++ Project 1/main.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include "SortedLinkedList.h"
 #include <fstream>
+#include <string>
 int fileReadingNumber;
 char userInputChar ;
 int userInputNum;
@@ -18,125 +19,117 @@ using namespace std;
 SortedLinkedList *list = new SortedLinkedList();
 
 void printCommands(){ // prints out the commands to the screen and waits for user input.
-    std::cout << "Commands,\n\n \t(i) - Insert value \n \t(d) - Delete value \n \t(f) - Search value \n \t(n) - Print iterator's next value \n \t(r) - Reset iterator \n \t(p) - Print list \n \t(l) - Print length \n \t(s) - Parewise Swap \n \t(c) - Clear List \n \t(q) - Quit Program \n\n";
+	std::cout << "Commands,\n\n \t(i) - Insert value \n \t(d) - Delete value \n \t(f) - Search value \n \t(n) - Print iterator's next value \n \t(r) - Reset iterator \n \t(p) - Print list \n \t(l) - Print length \n \t(s) - Parewise Swap \n \t(c) - Clear List \n \t(q) - Quit Program \n\n";
 }
 
 
-void fileHandler(){ // handles the file input.
-    ifstream inFile;
-    inFile.open("/Users/Home/Desktop/Project 1/input.txt");
-    if(!inFile){
-        cout << "Unable to open file\n";
-        exit(1);
-    }
-    else{
-        while (inFile >> fileReadingNumber){
-            DataType *input = new DataType(fileReadingNumber);
-            list->insertItem(*input);
-        }
-    }
-        inFile.close();
+void fileHandler(string input){ // handles the file input.
+	ifstream inFile;
+	inFile.open(input);
+	if(!inFile){
+		cout << "Unable to open file\n";
+		exit(1);
+	}
+	else{
+		while (inFile >> fileReadingNumber){
+			DataType *input = new DataType(fileReadingNumber);
+			list->insertItem(*input);
+		}
+	}
+		inFile.close();
 }
 
 void starter(){
-    DataType *startData = new DataType(0);
-    list->insertItem(*startData);
+	DataType *startData = new DataType(0);
+	list->insertItem(*startData);
 }
 
 void printBreakLine(){
-    std::cout<<"-------------------------------- \n";
-    
+	std::cout<<"-------------------------------- \n";
+	
 }
 
 int main(int argc, const char * argv[]) {
-    fileHandler(); // handles opening the file and creating an (unsorted) Linked List
-    printCommands(); // prints the commands and wait for user input.
-    continueLoop = 1;
-    list->printList();
-    while(continueLoop == 1){
-        std::cout << "Enter a command: ";
-        cin >> userInputChar;
-        switch (userInputChar) {
-        case 'i': {
-            list->printList();
-            std::cout << "What Number would you like to insert: ";
-            cin >> userInputNum;
-            DataType *itemToMake = new DataType(userInputNum);
-            list->insertItem(*itemToMake);
-            list->printList();
-            printBreakLine();
-            delete itemToMake;
-            break;
-        }
-        case 'd': {
-            list->printList();
-            std::cout << "What Number would you like to delete: ";
-            cin >> userInputNum;
-            DataType *itemToDelete = new DataType(userInputNum);
-            list->deleteItem(*itemToDelete);
-            list->printList();
-            printBreakLine();
-            delete itemToDelete;
-            break;
-        }
-            case 's': {
-            list->printList();
-            std::cout << "What Number would you like to search for: ";
-            cin >> userInputNum;
-            DataType *itemToSearch = new DataType(userInputNum);
-            std::cout << list->searchItem(*itemToSearch)<< " \n";
-            list->printList();
-            printBreakLine();
-            delete itemToSearch;
-            break;
-            }
-                
-            case 'n':   {
-                std::cout<<list->iterateList()<<"\n";
-                printBreakLine();
-            break;
-            }
-            case 'r': {
-                list->resetIterator();
-                printBreakLine();
-            break;
-            }
-                
-            case 'p':{
-                list->printList();
-                printBreakLine();
-            break;
-            }
-            case 'l':{
-                std::cout<<list->length()<<"\n";
-                printBreakLine();
-                break;
-            }
-            case 'c': {
-                list->clear();
-                printBreakLine();
-                break;
-            }
-            case 'b': {
-            
-                break;
-                }
-            case 'q':{
-                std::cout << "Good Bye";
-                continueLoop = 0;
-                break;
-            }
-            default:
-                std::cout << "That is not a valid Input please try again";
-                break;
-        }
-    }
-    exit(0);
+	string file = argv[1];
+	fileHandler(file); // handles opening the file and creating an (unsorted) Linked List
+	printCommands(); // prints the commands and wait for user input.
+	continueLoop = 1;
+	list->printList();
+	while(continueLoop == 1){
+		std::cout << "Enter a command: ";
+		cin >> userInputChar;
+		switch (userInputChar) {
+		case 'i': {
+			list->printList();
+			std::cout << "What Number would you like to insert: ";
+			cin >> userInputNum;
+			DataType *itemToMake = new DataType(userInputNum);
+			list->insertItem(*itemToMake);
+			list->printList();
+			printBreakLine();
+			delete itemToMake;
+			break;
+		}
+		case 'd': {
+			list->printList();
+			std::cout << "What Number would you like to delete: ";
+			cin >> userInputNum;
+			DataType *itemToDelete = new DataType(userInputNum);
+			list->deleteItem(*itemToDelete);
+			list->printList();
+			printBreakLine();
+			delete itemToDelete;
+			break;
+		}
+			case 's': {
+			list->printList();
+			std::cout << "What Number would you like to search for: ";
+			cin >> userInputNum;
+			DataType *itemToSearch = new DataType(userInputNum);
+			std::cout << list->searchItem(*itemToSearch)<< " \n";
+			list->printList();
+			printBreakLine();
+			delete itemToSearch;
+			break;
+			}
+				
+			case 'n':   {
+			
+			break;
+			}
+		  
+			case 'r': {
+			
+			break;
+			}
+		  
+			case 'p':{
+			
+			break;
+			}
+		
+			case 'l':{
+				break;
+			}
+			case 'c': {
+				
+				break;
+			}
+			case 'b': {
+			
+				break;
+				}
+			case 'q':{
+				continueLoop = 0;
+				break;
+			}
+			default:
+				std::cout << "That is not a valid Input please try again";
+				break;
+		}
+	}
+	exit(0);
 }
-
-
-
-
 
 
 
